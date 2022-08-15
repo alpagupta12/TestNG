7237c20f8f127014f9550356c519d6e31de095b3
Alpa Gupta <alpagupta@Alpas-MacBook-Pro.local>
2022-08-15 00:18:47 -0700

    updated
    locator added

--- src/test/java/WebDriverAutomation/Locators.java
+++ src/test/java/WebDriverAutomation/Locators.java
@@ -1,10 +1,13 @@
 package WebDriverAutomation;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Locators {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		WebDriverManager.chromedriver().setup();
 	}
 
 }
