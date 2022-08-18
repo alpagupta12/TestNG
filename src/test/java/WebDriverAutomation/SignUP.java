c630ba0427cce5c90134102d63f7f49913c36d30
alpagupta <alpagupta@localhost>
2022-08-18 00:53:53 -0700

    done

--- src/test/java/WebDriverAutomation/SignUP.java
+++ src/test/java/WebDriverAutomation/SignUP.java
@@ -16,27 +16,27 @@ public class SignUP {
 		WebDriver driver = new ChromeDriver();
 
 		driver.get("https://hotel.testplanisphere.dev/en-US/signup.html");
-		 Thread.sleep(2000);
+		Thread.sleep(2000);
 		WebElement emailtext = driver.findElement(By.xpath("//input[@id='email']"));
 
 		emailtext.sendKeys("testing@gmail.com");
 
-		 Thread.sleep(2000);
+		Thread.sleep(2000);
 
 		WebElement pwd = driver.findElement(By.id("password"));
 		pwd.sendKeys("Password123#");
 
-		 Thread.sleep(2000);
+		Thread.sleep(2000);
 
 		WebElement pwd_confirmation = driver.findElement(By.id("password-confirmation"));
 		pwd_confirmation.sendKeys("Password123#");
 
-		 Thread.sleep(2000);
+		Thread.sleep(2000);
 
 		WebElement name = driver.findElement(By.name("username"));
 		name.sendKeys("Alpa");
 
-		 Thread.sleep(3000);
+		Thread.sleep(3000);
 
 		// WebElement radio1 = driver.findElement(By.id("rank-premium"));
 		// radio1.click(); // Premium Membership
@@ -44,12 +44,12 @@ public class SignUP {
 		WebElement radio2 = driver.findElement(By.id("rank-normal"));
 		radio2.click(); // Membership
 
-		 Thread.sleep(3000);
+		Thread.sleep(3000);
 
 		WebElement address = driver.findElement(By.xpath("//input[@id='address']"));
 		address.sendKeys("1050 Duberry street, CA");
 
-		 Thread.sleep(3000);
+		Thread.sleep(3000);
 
 		WebElement number = driver.findElement(By.id("tel"));
 		number.sendKeys("09109679087");
@@ -61,22 +61,22 @@ public class SignUP {
 
 		gender_option.selectByIndex(2); // female
 
-		 Thread.sleep(3000);
+		Thread.sleep(3000);
 
 		WebElement DOB = driver.findElement(By.id("birthday"));
 		DOB.sendKeys("08/16/1991");
 
-		 Thread.sleep(3000);
+		Thread.sleep(3000);
 
 		WebElement notification = driver.findElement(By.id("notification"));
 		notification.click();
 
-		 Thread.sleep(3000);
+		Thread.sleep(3000);
 
 		WebElement signup = driver.findElement(By.xpath("//*[@id=\"signup-form\"]/button"));
 		signup.click();
 
-		 Thread.sleep(3000);
+		Thread.sleep(3000);
 
 		driver.close();
 		driver.quit();
